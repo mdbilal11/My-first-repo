325eac539e05984eb53b1f3023c487d5271a2b0c
mdbilal11 <mbilaakbar8@gmail.com>
2026-01-20 22:15:34 +0000

    Conversations of datatypes

--- .devcontainer/01_basics/03_conversationOperations.js
+++ .devcontainer/01_basics/03_conversationOperations.js
@@ -1,7 +1,25 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
+
+// "33"=> 33
+// "33abc" => NaN
+// ture => 1;
+// false => 0;
+
+let IsLoggedIn = "Bilal"
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Bilal" => ture
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+//console.log(stringNumber);
+console.log(typeof stringNumber);
