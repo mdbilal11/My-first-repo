384a061369906da3cc6046c9cde46b0e085e126e
mdbilal11 <mbilaakbar8@gmail.com>
2026-01-21 21:27:34 +0000

    operations

--- .devcontainer/01_basics/03_conversationOperations.js
+++ .devcontainer/01_basics/03_conversationOperations.js
@@ -22,4 +22,24 @@ let booleanIsLoggedIn = Boolean(IsLoggedIn)
 let someNumber = 33
 let stringNumber = String (someNumber)
 //console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+//************** Operations ************** */
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+let str1 = "Hello"
+let str2 = " Bilal"
+let str3 = str1 + str2
+
+console.log(str3);
+
