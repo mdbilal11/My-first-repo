fa46185f59eff0624c9e31b1966e4e758d2b1cd1
mdbilal11 <mbilaakbar8@gmail.com>
2026-01-27 20:20:39 +0000

    Number and maths

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(10));
+
+const othernum = 23.7169
+//console.log(othernum.toPrecision(2));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString(`en-IN`));
+
+//********** Maths **************/
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.1));
+// console.log(Math.min(4, 0, 9, 600));
+// console.log(Math.max(0, 4, 600, 9));
+
+
+// console.log(Math.random());
+// console.log(Math.random()*10 + 1);
+// console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1); // Agr aik hi number ma value chayie tu yeh asy use hoga.
+
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
+
