24825af4075d868f9211488860aaf31cfac2c58a
mdbilal11 <mbilaakbar8@gmail.com>
2026-02-10 10:46:31 +0000

    Revision

--- .devcontainer/01_basics/06_nums_and_math.js
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -34,5 +34,5 @@ const hundreds = 1000000
 const min = 10
 const max = 20 
 
-console.log(Math.floor(Math.random() * (max - min +1)) + min)
+// console.log(Math.floor(Math.random() * (max - min +1)) + min)
 
