864807c4c5fa0883f1e170392796a2fbae902642
mdbilal11 <mbilaakbar8@gmail.com>
2026-02-10 08:58:50 +0000

    Revision

--- .devcontainer/01_basics/03_conversationOperations.js
+++ .devcontainer/01_basics/03_conversationOperations.js
@@ -1,10 +1,10 @@
-let score = "33"
+let score = "33abc"
 
-//console.log(typeof score);
-//console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number (score)
-//console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 // "33"=> 33
 // "33abc" => NaN
@@ -13,7 +13,7 @@ let valueInNumber = Number (score)
 
 let IsLoggedIn = "Bilal"
 let booleanIsLoggedIn = Boolean(IsLoggedIn)
-//console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -21,8 +21,8 @@ let booleanIsLoggedIn = Boolean(IsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String (someNumber)
-//console.log(stringNumber);
-//console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 //************** Operations ************** */
 
