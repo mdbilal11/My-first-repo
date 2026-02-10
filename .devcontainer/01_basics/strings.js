935ec313ac617e4cdde7cfb02788b79b31496cbc
mdbilal11 <mbilaakbar8@gmail.com>
2026-02-10 08:09:29 +0000

    Again revision

--- .devcontainer/01_basics/strings.js
+++ .devcontainer/01_basics/strings.js
@@ -13,25 +13,25 @@ const gameName = new String (`Bilal-hc`)
 // String functions 
 
 //console.log(gameName.__proto__);
-//console.log(gameName.length);
-//console.log(gameName.toUpperCase());
-//console.log(gameName.charAt(0));
-//console.log(gameName.indexOf(`B`));
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf(`h`));
 
 const newString = gameName.substring (0, 4)
-console.log (newString); 
+// console.log (newString); 
 
 const anotherString = gameName.slice (-8, 7)
-console.log(anotherString);
+// console.log(anotherString);
 
 const myname = "   Bilal    "
 
-console.log(myname);
-console.log(myname.trim());
+// console.log(myname);
+// console.log(myname.trim());
 
 const url = "https://bilalagency.com%20gymwebsite"
-console.log(url.replace(`%20`, `/`));
+// console.log(url.replace(`%20`, `/`));
 
-console.log(url.includes(`Numan`));
+// console.log(url.includes(`Numan`));
 
-console.log(gameName.split(`l`));
+// console.log(gameName.split(`l`));
