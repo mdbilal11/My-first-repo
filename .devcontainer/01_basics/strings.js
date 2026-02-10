c787cb841606807a101442bd8adb0891c1d4488a
mdbilal11 <mbilaakbar8@gmail.com>
2026-02-10 08:03:49 +0000

    Revision

--- .devcontainer/01_basics/strings.js
+++ .devcontainer/01_basics/strings.js
@@ -3,7 +3,7 @@ const repoCount = 40
 
 //console.log(name + repoCount + " Value"); This is old and normal way.
 
-//console.log(`Hello my name is ${name} and my repo count is ${repoCount} `); // This way is called
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount} `); This way is called
 // string interpulation.
 
 const gameName = new String (`Bilal-hc`)
@@ -19,19 +19,19 @@ const gameName = new String (`Bilal-hc`)
 //console.log(gameName.indexOf(`B`));
 
 const newString = gameName.substring (0, 4)
-//console.log (newString); 
+console.log (newString); 
 
-const anotherString = gameName.slice (-8, 4)
-//console.log(anotherString);
+const anotherString = gameName.slice (-8, 7)
+console.log(anotherString);
 
 const myname = "   Bilal    "
-//
-//console.log(myname);
-//console.log(myname.trim());
+
+console.log(myname);
+console.log(myname.trim());
 
 const url = "https://bilalagency.com%20gymwebsite"
-//console.log(url.replace(`%20`, `/`));
+console.log(url.replace(`%20`, `/`));
 
-//console.log(url.includes(`Numan`));
+console.log(url.includes(`Numan`));
 
-console.log(gameName.split(`-`));
+console.log(gameName.split(`l`));
