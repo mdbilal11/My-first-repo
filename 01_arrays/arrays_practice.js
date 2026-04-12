908fed206e3f5fbec1bfa97b2dc6f6923cbe5781
mdbilal11 <mbilaakbar8@gmail.com>
2026-04-12 20:09:49 +0000

    Arrays Practice

---
+++ 01_arrays/arrays_practice.js
@@ -0,0 +1,32 @@
+const fruits = ["apple", "banana", "cherry"]
+console.log(fruits);
+console.log("Type:", typeof fruits);
+console.log("Is array?:", Array.isArray (fruits))
+
+
+// Arrays can hold anything 
+
+const mixed = [2, true, "Bilal", null]
+console.log(mixed);
+console.log("Type:", typeof mixed);
+console.log("Is array?", Array.isArray (mixed));
+
+
+
+
+
+
+// const arr = []
+// console.log(arr);
+
+// const arr2 = [1, 3, 4]
+// console.log(arr2);
+
+// const mixed_arr = ["hi", 23, true, false, null]
+// console.log(mixed_arr);
+
+// const again_arr = [[1,2], [3, 4]]
+// console.log(again_arr);
+
+
+
