f0a95af9e4b86454b8e2a1330a572233c7a0a824
mdbilal11 <mbilaakbar8@gmail.com>
2026-04-16 16:01:33 +0000

    Objects

---
+++ 01_arrays/03_objects.js
@@ -0,0 +1,30 @@
+// Singleton
+
+// object literals
+Object.create  // This is constractor method.
+
+
+const JsUser = {}  // Yeh ban gya hai object.
+
+const JsUser1  = {
+    name: "Bilal",
+    age: 19,
+    Location: "FSD",
+    Email: "bilal@amazon.com",
+    IsLoggedIn: false,
+    LastLoginDays: ["Monday", "Tuesday"]
+
+}
+
+// One Method to access the values
+
+console.log(JsUser1.name);
+console.log(JsUser1.age);
+console.log(JsUser1.Location);
+console.log(JsUser1.LastLoginDays);
+console.log(JsUser1.IsLoggedIn);
+
+
+// 2nd and best method to Access Values
+
+console.log(JsUser1["name", "age", "Location"])
