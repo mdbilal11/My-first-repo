c32586b4a95e43666009f29c15a2ccb0e6bbe73f
mdbilal11 <mbilaakbar8@gmail.com>
2026-05-26 20:34:17 +0000

    Code learning

--- 01_arrays/01_arrays.js
+++ 01_arrays/01_arrays.js
@@ -4,16 +4,16 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeros = ["Imran Khan", "Akbar", "Ali"]
 
 const myArr2 = new Array (0, 1, 2, 3, 4, 5)
-// console.log(myArr[1]);
+// console.log(myHeros[1]);
 
 // Arrays methods
 
 // myArr.push(6)    // Value add karta hai end py
 // myArr.push(7)    
-console.log(myArr);
+// console.log(myArr.push(7));
 
 // myArr.pop()     // Value remove karta hai end wali 
-// console.log(myArr);
+// console.log(myArr.pop());
 
 // myArr.unshift(5)
 // myArr.shift()
@@ -28,14 +28,14 @@ console.log(myArr);
 
 // Slice and Splice 
 
-console.log("A", myArr);
+// console.log("A", myArr);
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B", myArr);
+// console.log(myn1);
+// console.log("B", myArr);
 
 const myn2 = myArr.splice(1,3)
-console.log("C", myArr);
-console.log(myn2);
+// console.log("C", myArr);
+// console.log(myn2);
 
