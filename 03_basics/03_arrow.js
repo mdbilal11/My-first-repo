88448fe8c176e206484b7a9eddd5d54c2380dc76
mdbilal11 <mbilaakbar8@gmail.com>
2026-06-07 12:39:32 +0000

    Arrow learn today

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -20,4 +20,27 @@ function ice() {
     console.log(this);
 }
 
-ice()
+// ice()
+
+// This is arrow function 
+
+// () => {}
+
+// const addTwo = (num1, num2) => {                agr yeh curly braces use karey tu "return" likhna hii parey ga.
+//     return num1 + num2
+// }
+
+// console.log(addTwo(3,4));
+
+// const addTwo = (num1, num2) => num1 + num2      Yaha return likhny ki zarorat nhii hai.
+// const addTwo = (num1, num2) => (num1 + num2)    Agr yeh parenthesis use karni hai tu tab bi "return" nhi likhna
+const addTwo = (num1, num2) => ({username: "bilal"}) 
+
+
+console.log(addTwo(3,4));
+
+
+const myArray = [2, 3, 4, 5, 6]
+
+myArray.forEach(funtion () , {});
+myArray.forEach(() => {}) 
