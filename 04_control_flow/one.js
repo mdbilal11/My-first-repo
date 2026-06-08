6d83143cdd2bd2e36c3791d59d9b3b7b15b54cdf
mdbilal11 <mbilaakbar8@gmail.com>
2026-06-08 21:24:44 +0000

    Control flow learning

---
+++ 04_control_flow/one.js
@@ -0,0 +1,31 @@
+// if
+
+// const temperature = 40;
+
+// if (temperature < 42) {
+//     console.log("greater than 40 degree");
+
+// } else {
+//     console.log("temperature is less than 40");
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==          Operators.
+
+// const score = 200;
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+
+const balance = 1000
+
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+} else if (balance < 900) {
+    console.log("less than 900");
+} else {
+    console.log("less than 1500");
+}
