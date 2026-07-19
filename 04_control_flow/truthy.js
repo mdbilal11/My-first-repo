154d21dece5520ed004ef259c7ff1a87392fec2b
mdbilal11 <mbilaakbar8@gmail.com>
2026-07-19 05:08:04 +0000

    Truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,23 @@
+// const UserEmail = {};
+
+// if (UserEmail) {
+//     console.log("Got the user Email");
+// } else {
+//     console.log("Don't have user email");
+// }
+
+// falsy values 
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+
+// "0", "false", 'false', " ", [], {}, function(){}, 
+
+// Nullish Coaleacing Operator (??) : null defined
+
+val1 = null ?? 12 ?? 10
+
+
+
+console.log(val1)
