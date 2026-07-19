78d0826dc38434f164ec701591175b9e29b6bfec
mdbilal11 <mbilaakbar8@gmail.com>
2026-07-19 04:13:22 +0000

    One file

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -2,7 +2,7 @@
 
 // const temperature = 40;
 
-// if (temperature < 42) {
+// if (temperature < 41) {
 //     console.log("greater than 40 degree");
 
 // } else {
@@ -18,14 +18,30 @@
 //     console.log(`User Power: ${power}`);
 // }
 
-const balance = 1000
+// const balance = 1000
 
-if (balance < 500) {
-    console.log("less than 500");
-} else if (balance < 750) {
-    console.log("less than 750");
-} else if (balance < 900) {
-    console.log("less than 900");
-} else {
-    console.log("less than 1500");
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else if (balance = 1200){
+    // console.log("less than 5000");
+// }
+//  else {
+    // console.log("less than 1000");
+// }
+
+
+const gmailLoggedIn = true;
+const debitCard = true;
+const LoggedInFromGoogle = true; 
+
+if (gmailLoggedIn && debitCard) {
+    console.log("Allow to buy the course.");
+}
+
+if (gmailLoggedIn || LoggedInFromGoogle) {
+    console.log("Allow")
 }
