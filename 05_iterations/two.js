7c7919f1efe36a340f56b8a1fa808011fa464d21
mdbilal11 <mbilaakbar8@gmail.com>
2026-07-20 11:29:25 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+// let cash = 1
+
+// while (cash <= 2) {
+//     console.log("Cash is: " + cash);
+//     cash++; // Increment Operator
+// }
+
+let count = 5
+
+while (count <=50) {
+    console.log(`The count valus is ${count}`);
+    count = count + 50
+    
+}
