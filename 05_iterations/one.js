4b0493c89847b69bd8bf4e8a3f0e5d49eef80fc0
mdbilal11 <mbilaakbar8@gmail.com>
2026-07-20 08:15:44 +0000

    for loop learn

---
+++ 05_iterations/one.js
@@ -0,0 +1,37 @@
+// for
+
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     console.log(element);
+
+    
+// }
+
+// let myArray = ["spiderman", "batman", "superman"]
+// console.log(myArray.length)
+
+// for (let index = 0; index <= myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element)
+    
+// }
+
+// break and continue 
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`Value of i is ${index}`);
+    
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
